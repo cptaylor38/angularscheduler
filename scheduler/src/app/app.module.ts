8379d33da71755d5e78c41f28e9a8f9e98bbb4f8
Corbin Taylor <cptaylor38@yahoo.com>
2021-01-27 15:32:12 -0500

    mapping layout for consideration while reviewing course

--- scheduler/src/app/app.module.ts
+++ scheduler/src/app/app.module.ts
@@ -3,21 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {TestComponent} from './Components/Test/Test.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
